224f9a8a23d50ee6bdc06a9fa4dd30a43a860c27
denicabaravdic <deni@phntmlabs.com>
2023-04-16 18:00:12 -0700

    added flask connections

---
+++ BackendV2/app.py
@@ -0,0 +1,112 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+import sqlite3
+import os
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+db_path = 'phase1.sqlite'
+
+app.config['FLASH_CATEGORY'] = 'now'
+
+def init_database():
+    if not os.path.exists(db_path):
+        conn = sqlite3.connect(db_path)
+        conn.execute('''CREATE TABLE IF NOT EXISTS users (
+                        username TEXT PRIMARY KEY,
+                        email TEXT,
+                        firstName TEXT,
+                        lastName TEXT,
+                        password TEXT
+                        )''')
+        conn.execute('''CREATE TABLE IF NOT EXISTS items (
+                        username TEXT,
+                        title TEXT,
+                        description TEXT,
+                        category TEXT,
+                        price TEXT,
+                        date TEXT
+                        )''')
+        conn.commit()
+        conn.close()
+
+@app.route
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if request.method == 'POST':
+        action = request.form.get('action')
+        if action == 'signin':
+            return handle_signin()
+        elif action == 'signup':
+            return redirect(url_for('handle_signup'))
+    return redirect(url_for('handle_signin'))
+
+
+@app.route('/signin', methods=['GET', 'POST'])
+def handle_signin():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        with sqlite3.connect(db_path) as conn:
+            c = conn.cursor()
+            c.execute('SELECT * FROM users WHERE username=? AND password=?', (username, password))
+            user = c.fetchone()
+            if user is None:
+                flash('Invalid username or password!', app.config['FLASH_CATEGORY'])
+            else:
+                flash('Sign in successful!', app.config['FLASH_CATEGORY'])
+                return redirect(url_for('profile', firstName=user[2], lastName=user[3]))
+    return render_template('signin.html')
+
+
+@app.route('/profile/<firstName>/<lastName>')
+def profile(firstName, lastName):
+    return render_template('profile.html', name=firstName + " " + lastName)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def handle_signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        password = request.form['password']
+        confirmPassword = request.form['confirmPassword']
+
+        with sqlite3.connect(db_path) as conn:
+            c = conn.cursor()
+
+            c.execute('SELECT * FROM users WHERE username = ?', (username,))
+            if c.fetchall():
+                flash('Username already exists!', app.config['FLASH_CATEGORY'])
+                return redirect(url_for('handle_signup'))
+
+            c.execute('SELECT * FROM users WHERE email = ?', (email,))
+            if c.fetchall():
+                flash('Email already exists!', app.config['FLASH_CATEGORY'])
+                return redirect(url_for('handle_signup'))
+
+            if password != confirmPassword:
+                flash('Passwords do not match!', 'danger', app.config['FLASH_CATEGORY'])
+                return redirect(url_for('handle_signup'))
+
+            c.execute('''INSERT INTO users (username, email, firstName, lastName, password) 
+                          VALUES (?, ?, ?, ?, ?)''', (username, email, firstName, lastName, password))
+            conn.commit()
+
+            flash('Registration successful!', app.config['FLASH_CATEGORY'])
+            return redirect(url_for('handle_signin'))
+
+    return render_template('signup.html')
+
+
+
+@app.route('/searchbar', methods=['GET', 'POST'])
+def searchbar():
+    return render_template('searchbar.html')
+
+
+if __name__ == '__main__':
+    init_database()
+    app.run(debug=True)
