e89a527cf546326a55b338959cdf5152d0853730
denicabaravdic <deni@phntmlabs.com>
2023-05-03 21:07:48 -0700

    styling changes

--- static/signin.js
+++ static/signin.js
@@ -1,5 +1,7 @@
 const labels = document.querySelectorAll('.form-control label')
 
+
+
 labels.forEach(label => {
     label.innerHTML = label.innerText
         .split('') //Splits letters into an array
